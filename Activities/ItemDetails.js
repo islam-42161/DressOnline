ef657f975234d236d8986d3f5cdf0bbf680f39a4
Muhammad Sazzadul Islam <mdsazzadul.islam.42161@gmail.com>
2022-06-08 23:33:12 +0600

    kisui pari na :(

--- Activities/ItemDetails.js
+++ Activities/ItemDetails.js
@@ -38,8 +38,8 @@ const ItemDetails = ({ route, navigation }) => {
       <SafeAreaView style={styles.container}>
         {/* Top image */}
         <View style={{marginTop:Platform.OS === "android" ? SB.currentHeight : 0}}>
-          <TouchableOpacity style={{zIndex:1,position:'absolute',top:4*PADDING,right:PADDING,backgroundColor:'white', padding:PADDING, borderRadius:20,alignItems:'center',elevation:PADDING}}>
-          <MaterialCommunityIcons name="cards-heart" size={30} color="red" />
+          <TouchableOpacity style={{zIndex:1,position:'absolute',bottom:4*PADDING,right:PADDING,backgroundColor:'white', padding:PADDING, borderRadius:25,alignItems:'center',elevation:PADDING}}>
+          <MaterialCommunityIcons name="cards-heart" size={35} color="red" />
           </TouchableOpacity>
           <Image source={{ uri: item.image }} resizeMode={'contain'} style={{ width:width*0.95, height: TOP_HEADER_HEIGHT-statusBarHeight}} />
         </View>
@@ -58,14 +58,14 @@ const ItemDetails = ({ route, navigation }) => {
       <View style={{backgroundColor:'lightgray',height:1,width:width*0.95,marginVertical:PADDING}}/>
 
           {/* description and item add button */}
-          <Text style={{fontSize:15,fontWeight:'bold',marginTop:PADDING}}>Details:</Text>
-          <View style={{ flexDirection: 'row',alignItems:'center'}}>
+          <Text style={{fontSize:16,fontWeight:'700',marginTop:PADDING}}>Details:</Text>
+          <View style={{ height: (height - TOP_HEADER_HEIGHT) * 0.35,flexDirection: 'row',alignItems:'center'}}>
 
 {/* Details text */}
-<View style={{height: (height - TOP_HEADER_HEIGHT) * 0.35,width: width * 0.8,alignSelf:'flex-start'}}>
+<View style={{width: width * 0.8,alignSelf:'flex-start',paddingBottom:PADDING*2}}>
             <ScrollView showsVerticalScrollIndicator={false}>
 
-                <ReadMore numberOfLines={5} style={{lineHeight:20,paddingVertical:PADDING}}>
+                <ReadMore numberOfLines={4} style={{lineHeight:25,paddingVertical:PADDING,fontSize:16}}>
                 {item.description}
                 </ReadMore>
 
@@ -74,47 +74,44 @@ const ItemDetails = ({ route, navigation }) => {
             </View>
 
             <View style={{ alignItems: 'center',flex:1,padding:PADDING}}>
-              <View style={{alignItems:'center',backgroundColor:'lightgray',borderRadius:15,elevation:PADDING*2,alignSelf:'flex-end'}}>
+              <View style={{alignItems:'center',backgroundColor:'#F3F3F3',borderRadius:15,elevation:PADDING*2,alignSelf:'flex-end'}}>
               <TouchableOpacity style={{ alignItems: 'center', backgroundColor: 'white', borderTopLeftRadius: 15, borderTopRightRadius: 15 }} onPress={()=>{
                 setItemNumber(itemNumber+1);
               }}>
-                <MaterialCommunityIcons name="plus" size={34} color="black" />
+                <MaterialCommunityIcons name="plus" size={50} color="black" />
               </TouchableOpacity>
 
-              <Text style={{fontWeight:'600',fontSize:20}}>{itemNumber}</Text>
+              <Text style={{fontWeight:'600',fontSize:30}}>{itemNumber}</Text>
 
               <TouchableOpacity style={{ alignItems: 'center', backgroundColor: 'white', borderBottomLeftRadius: 15, borderBottomRightRadius: 15 }} onPress={()=>{
                 itemNumber>0?setItemNumber(itemNumber-1):null;
               }}>
-                <MaterialCommunityIcons name="minus" size={34} color="black" />
+                <MaterialCommunityIcons name="minus" size={50} color="black" />
               </TouchableOpacity>
             </View>
               </View>
           </View>
+
         </View>
-
-
 {/* Bottom buttons */}
 <View style={{
   justifyContent:'center',
   position:'absolute',
-  bottom:0,
+  bottom:PADDING,
   }}>
   <View style={{flexDirection:'row',justifyContent:'center'}}>
 
-  {itemNumber>0?(
-    <TouchableOpacity activeOpacity={0.6} style={{marginBottom:PADDING,flexDirection:'row',alignItems:'center',justifyContent:'space-evenly',borderRadius:6,backgroundColor:'black',padding:PADDING,marginHorizontal:PADDING,elevation:PADDING}}>
-    <MaterialCommunityIcons name="cart-check" size={24} color="white" />
-    <Text style={{marginHorizontal:PADDING,color:'white',fontWeight:'800'}}>Checkout Now</Text>
-    <Text style={{marginHorizontal:PADDING,color:'white',letterSpacing:PADDING/2}}>${(item.price * itemNumber).toFixed(2)}</Text>
+
+    <TouchableOpacity activeOpacity={0.6} style={{marginBottom:PADDING,flexDirection:'row',alignItems:'center',justifyContent:'space-evenly',borderRadius:6,backgroundColor:itemNumber>0?'white':'lightgray',padding:PADDING,marginHorizontal:PADDING,elevation:itemNumber>0?PADDING:0}} disabled={itemNumber>0?false:true}>
+    <MaterialCommunityIcons name="cart" size={24} color={itemNumber>0?'black':'gray'} />
+    <Text style={{marginHorizontal:PADDING,color:'black',fontWeight:'800',color:itemNumber>0?'black':'gray'}}>Checkout Now</Text>
+    <Text style={{marginHorizontal:PADDING,color:'black',letterSpacing:PADDING/2,color:itemNumber>0?'black':'gray'}}>${(item.price * itemNumber).toFixed(2)}</Text>
   </TouchableOpacity>
-  )
-  :
-  null
-  }
+
 
   </View>
 </View>
+
       </SafeAreaView>
 
     )
